117353662e6f8ddb4f68d35253c8017a8b265e13
B10956026 <84659977+B10956026@users.noreply.github.com>
2022-05-10 22:24:30 +0800

    加入專案檔案。

---
+++ Models/TestDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace _20220509.Models
+{
+    public class TestDB : DbContext
+    {
+        public TestDB(DbContextOptions<TestDB> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
